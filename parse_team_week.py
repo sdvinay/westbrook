70aa74c836dd636bdf05572733eee437c7251e00
Vinay Kumar <sdvinay@gmail.com>
2009-11-30 00:23:52 -0800

    Better document parse_team_week.py

--- parse_team_week.py
+++ parse_team_week.py
@@ -4,13 +4,22 @@ import xml.dom.minidom
 import csv
 import sys
 
-wrtr = csv.writer(sys.stdout)
+# Parses team-week data as retrieved by the Yahoo FFL iPhone APIs.
+# Reads in files based on a known directory/filename layout.
+
+# For each file, extracts each player as a CSV row in this format:
+#  team, week, playerID, playerFullname, lineupPosition, fantasyPoints
 
 # TODO this assumes that the data is in xml files under data/
-# TODO assumes 12 teams
-# TODO assumes 12 weeks
-for tm in range(1, 13) : 
-	for wk in range(1, 13) : 
+# assumes that each file is named data/stats_<team>_<week>.xml
+
+numTeams = 12
+numWeeks = 12
+
+wrtr = csv.writer(sys.stdout)
+
+for tm in range(1, numTeams+1) : 
+	for wk in range(1, numWeeks+1) : 
 		filename = "data/stats_" + format(tm) + "_" + format(wk) + ".xml"
 		mydoc = xml.dom.minidom.parse(filename)
 		plrs = mydoc.getElementsByTagName("fantasy-player")
